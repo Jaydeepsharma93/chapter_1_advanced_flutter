5dd69f534ffc4d82d1d45b15dd7abd9d68083ac8
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-05-02 13:53:43 +0530

    2-05

---
+++ lib/screens/1.6 Contact Us Page With Interaction/view/url_Interaction.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Interaction extends StatelessWidget {
+  const Interaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
